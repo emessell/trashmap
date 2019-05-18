d82a0dd396b0cbb6ea5225f52de8277c9cdca4fb
emessell <32255929+emessell@users.noreply.github.com>
2019-05-18 12:57:31 +0900

    2019/05/18

--- src/view/App_header.js
+++ src/view/App_header.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../img/Logo.png';
 
 console.log(logo);
-
-function App_header() {
-  return (
-    <div className="App_header">
-      <img src='{logo}'></img>
-      <a className='nav-item' href='/'>A</a>
-      <a className='nav-item' href='/'>B</a>
-      <a className='nav-item' href='/'>C</a>
-    </div>
-  );
+class App_header extends Component{
+  render(){
+    return (
+      <div className="App_header">
+        <a className='nav-item' href='/'>A</a>
+        <a className='nav-item' href='/'>B</a>
+        <a className='nav-item' href='/'>C</a>
+      </div>
+    );
+  }
 }
+
 
 export default App_header;
