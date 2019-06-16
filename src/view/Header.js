a736efbcfbcc3c98adcbfbcd089b121fc71a594c
emessell <32255929+emessell@users.noreply.github.com>
2019-06-16 16:07:23 +0900

    temp2

--- src/view/Header.js
+++ src/view/Header.js
@@ -5,9 +5,14 @@ class Header extends Component{
   render(){
     return (
       <div className="Header">
-        <a href='/'>
+        <a href='/' className='goHome'>
           <img className='logo' src={logo} alt="logo" />
         </a>
+        <ul className='nav'>
+            <li>To a</li>
+            <li>To b</li>
+            <li>To c</li>
+        </ul>
       </div>
     );
   }
